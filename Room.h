ab7319e6520bb1e9f1797abd780b0b91fca4e34b
shaddowlinkk <nathan.vanbrunt@gmail.com>
2020-09-25 22:28:18 -0400

    bit sized

--- Room.h
+++ Room.h
@@ -8,10 +8,10 @@
 #include <stdlib.h>
 #include <time.h>
 typedef struct _room{
-    unsigned int door;
+    unsigned int door:4;
     char *mapName;
     struct _room *rooms[4];
-    int gened;
+    int gened:1;
 }Room;
 #define INIT_ROOM_PTR(x) x->door=0,x->mapName="",x->rooms[0]=NULL,x->rooms[1]=NULL,x->rooms[2]=NULL,x->rooms[3]=NULL,x->gened=0;
 #define INIT_ROOM_ARRAY(x) x[0]=NULL,x[1]=NULL,x[2]=NULL,x[3]=NULL;
