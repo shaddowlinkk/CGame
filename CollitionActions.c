a7498771c4cb687a3290a24f89af610ecc4e5f9b
shaddowlinkk <nathan.vanbrunt@gmail.com>
2020-12-23 17:02:15 -0500

    mathed things

--- CollitionActions.c
+++ CollitionActions.c
@@ -5,6 +5,26 @@
 #include "CollitionActions.h"
 #include "BoundingboxUtil.h"
 
+/*TODO Give point p¬1,p1 get Δx and  Δy to move the box away
+NEED TO IMPLEMENT
+Xp1,p2= (xp1-xp2)
+yp1,p2= (yp1-yp2)
+θ = tan-1(yp1,p2/Xp1,p2)
+HR =(HR∈ℝ|(π/2)>HR>-( π/2) ⋃ 1 ≤ tan(HR)≥ -1 )
+HL =(HL∈ℝ|(π/2)<HR<-( π/2) ⋃ 1 ≤ tan(HR)≥ -1 )
+
+VUR =( VUR ∈ℝ|(π/2)> VUR >-( π/2) ⋃ 1 ≥ tan(VUR)  )
+VDR=(VL∈ℝ|(π/2)> VUR >-( π/2) ⋃ tan(VDR) ≤ -1 )
+
+VUL =( VUR ∈ℝ|(π/2)> VUL >-( π/2) ⋃ 1 ≥ tan(VDL-( π/2))  )
+VDL=(VDL∈ℝ|(π/2)> VDL >-( π/2) ⋃ tan(tan(VDL+( π/2)) ≤ -1 )
+
+
+Δx ={HR ⋂ θ then 1, HL ⋂ θ then -1 else 0 }
+Δy ={(VUR⋀VUL) ⋂ θ then -1, (VDR⋀VDL) ⋂ θ then 1 else 0 }*/
+
+
+
 void stopEntityMovement(Entity *mover,int side){
     if(side==0){
         mover->sprite.y=mover->sprite.y-5;
